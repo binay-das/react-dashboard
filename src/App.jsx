d91eaa45a8c51b9a29d006385fee0c3393e3a2e3
Binay <binaydas1123@gmail.com>
2024-10-23 12:56:54 +0530

    Few changes in UI

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,7 @@ import SettingsPage from "./pages/SettingsPage";
 
 function App() {
 	return (
-		<div className='flex h-screen bg-gray-900 bg-black text-gray-100 overflow-hidden'>
-			
-			{/* <div className='fixed inset-0 z-0'> */}
-				{/* <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' /> */}
-				{/* <div className='absolute inset-0 backdrop-blur-sm' /> */}
-			{/* </div> */}
-
+		<div className='flex h-screen bg-black text-gray-100 overflow-hidden'>
 			<Sidebar />
 			<Routes>
 				<Route path='/' element={<Dashboard />} />
--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
 				}`}
 			animate={{ width: isSidebarOpen ? 256 : 80 }}
 		>
-			<div className='h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700'>
+			<div className='h-full bg-black bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700'>
 				<motion.button
 					whileHover={{ scale: 1.1 }}
 					whileTap={{ scale: 0.9 }}
