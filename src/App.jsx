13db87b64ea0d3413a04847cef15a37315405b61
Binay <binaydas1123@gmail.com>
2024-10-23 12:49:17 +0530

    version 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+
+import Sidebar from "./components/common/Sidebar";
+
+import Dashboard from "./pages/Dashboard";
+import ProductsPage from "./pages/ProductsPage";
+import Leads from "./pages/Leads";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<div className='flex h-screen bg-gray-900 bg-black text-gray-100 overflow-hidden'>
+			
+			{/* <div className='fixed inset-0 z-0'> */}
+				{/* <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' /> */}
+				{/* <div className='absolute inset-0 backdrop-blur-sm' /> */}
+			{/* </div> */}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/leads' element={<Leads />} />
+				<Route path='/analytics' element={<AnalyticsPage />} />
+				<Route path='/products' element={<ProductsPage />} />
+				<Route path='/reports' element={<SalesPage />} />
+				<Route path='/orders' element={<OrdersPage />} />
+				<Route path='/settings' element={<SettingsPage />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
