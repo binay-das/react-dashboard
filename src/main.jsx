ddb797aa7b3c8a9552354f8cc9688d037fd0607e
Binay <binaydas1123@gmail.com>
2025-03-23 22:59:49 +0530

    Routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <BrowserRouter > */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </StrictMode>
 );
